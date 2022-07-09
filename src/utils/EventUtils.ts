d7f0a354f9180d4e6084319610cdcc047a5a7c2c
Patrick Organ <patrick@permafrost.dev>
2022-07-09 10:09:01 -0400

    refactoring

--- src/utils/EventUtils.ts
+++ src/utils/EventUtils.ts
@@ -149,47 +149,13 @@ export interface Comment {
     issue_url: string;
     id: number;
     node_id: string;
-    user: User2;
+    user: User;
     created_at: string;
     updated_at: string;
     author_association: string;
     body: string;
-    reactions: Reactions2;
+    reactions: Reactions;
     performed_via_github_app: any;
-}
-
-export interface User2 {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-}
-
-export interface Reactions2 {
-    url: string;
-    total_count: number;
-    '+1': number;
-    '-1': number;
-    laugh: number;
-    hooray: number;
-    confused: number;
-    heart: number;
-    rocket: number;
-    eyes: number;
 }
 
 export interface PullRequest2 {
@@ -204,7 +170,7 @@ export interface PullRequest2 {
     state: string;
     locked: boolean;
     title: string;
-    user: User3;
+    user: User;
     body: string;
     created_at: string;
     updated_at: string;
@@ -215,7 +181,7 @@ export interface PullRequest2 {
     assignees: any[];
     requested_reviewers: any[];
     requested_teams: any[];
-    labels: Label2[];
+    labels: Label[];
     milestone: any;
     draft: boolean;
     commits_url: string;
@@ -233,7 +199,7 @@ export interface PullRequest2 {
     mergeable?: boolean;
     rebaseable?: boolean;
     mergeable_state: string;
-    merged_by?: MergedBy;
+    merged_by?: User;
     comments: number;
     review_comments: number;
     maintainer_can_modify: boolean;
@@ -243,37 +209,6 @@ export interface PullRequest2 {
     changed_files: number;
 }
 
-export interface User3 {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-}
-
-export interface Label2 {
-    id: number;
-    node_id: string;
-    url: string;
-    name: string;
-    color: string;
-    default: boolean;
-    description: string;
-}
-
 export interface Head {
     label: string;
     ref: string;
@@ -281,35 +216,13 @@ export interface Head {
     user: User;
     repo: Repo2;
 }
-
-export interface User4 {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-}
-
 export interface Repo2 {
     id: number;
     node_id: string;
     name: string;
     full_name: string;
     private: boolean;
-    owner: Owner;
+    owner: User;
     html_url: string;
     description: string;
     fork: boolean;
@@ -383,28 +296,6 @@ export interface Repo2 {
     watchers: number;
     default_branch: string;
 }
-
-export interface Owner {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-}
-
 export interface License {
     key: string;
     name: string;
@@ -421,170 +312,19 @@ export interface Base {
     repo: Repo;
 }
 
-export interface User5 {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-}
-
-export interface Repo3 {
-    id: number;
-    node_id: string;
-    name: string;
-    full_name: string;
-    private: boolean;
-    owner: Owner;
-    html_url: string;
-    description: string;
-    fork: boolean;
-    url: string;
-    forks_url: string;
-    keys_url: string;
-    collaborators_url: string;
-    teams_url: string;
-    hooks_url: string;
-    issue_events_url: string;
-    events_url: string;
-    assignees_url: string;
-    branches_url: string;
-    tags_url: string;
-    blobs_url: string;
-    git_tags_url: string;
-    git_refs_url: string;
-    trees_url: string;
-    statuses_url: string;
-    languages_url: string;
-    stargazers_url: string;
-    contributors_url: string;
-    subscribers_url: string;
-    subscription_url: string;
-    commits_url: string;
-    git_commits_url: string;
-    comments_url: string;
-    issue_comment_url: string;
-    contents_url: string;
-    compare_url: string;
-    merges_url: string;
-    archive_url: string;
-    downloads_url: string;
-    issues_url: string;
-    pulls_url: string;
-    milestones_url: string;
-    notifications_url: string;
-    labels_url: string;
-    releases_url: string;
-    deployments_url: string;
-    created_at: string;
-    updated_at: string;
-    pushed_at: string;
-    git_url: string;
-    ssh_url: string;
-    clone_url: string;
-    svn_url: string;
-    homepage: any;
-    size: number;
-    stargazers_count: number;
-    watchers_count: number;
-    language: string;
-    has_issues: boolean;
-    has_projects: boolean;
-    has_downloads: boolean;
-    has_wiki: boolean;
-    has_pages: boolean;
-    forks_count: number;
-    mirror_url: any;
-    archived: boolean;
-    disabled: boolean;
-    open_issues_count: number;
-    license: License;
-    allow_forking: boolean;
-    is_template: boolean;
-    web_commit_signoff_required: boolean;
-    topics: any[];
-    visibility: string;
-    forks: number;
-    open_issues: number;
-    watchers: number;
-    default_branch: string;
-}
-
 export interface Links {
-    self: Self;
-    html: Html;
-    issue: Issue2;
-    comments: Comments;
-    review_comments: ReviewComments;
-    review_comment: ReviewComment;
-    commits: Commits;
-    statuses: Statuses;
+    self: Link;
+    html: Link;
+    issue: Link;
+    comments: Link;
+    review_comments: Link;
+    review_comment: Link;
+    commits: Link;
+    statuses: Link;
 }
 
-export interface Self {
+export interface Link {
     href: string;
-}
-
-export interface Html {
-    href: string;
-}
-
-export interface Issue2 {
-    href: string;
-}
-
-export interface Comments {
-    href: string;
-}
-
-export interface ReviewComments {
-    href: string;
-}
-
-export interface ReviewComment {
-    href: string;
-}
-
-export interface Commits {
-    href: string;
-}
-
-export interface Statuses {
-    href: string;
-}
-
-export interface MergedBy {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
 }
 
 export interface Org {
