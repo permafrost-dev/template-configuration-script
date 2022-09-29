6f1114cf08b0806e7bb7bb66c2dae3a8409dd797
Patrick Organ <patrick@permafrost.dev>
2022-09-29 07:01:08 -0400

    add getUrl() for retrieving a url with any content type

--- src/utils/HttpUtils.ts
+++ src/utils/HttpUtils.ts
@@ -68,3 +68,61 @@ export async function getJson(url: string): Promise<RestApiResponse> {
 
     return result;
 }
+
+export async function getUrl(url: string): Promise<RestApiResponse> {
+    const result: RestApiResponse = {
+        data: null,
+        error: '',
+        failed: false,
+        hasError: false,
+        headers: {},
+        statusCode: 0,
+        success: false,
+    };
+
+    const requestContent = async url => {
+        const options = {
+            headers: {
+                Accept: 'text/plain, */*',
+                'User-Agent': 'permafrost-dev-template-configure/1.0',
+            },
+        };
+
+        return new Promise((resolve, reject) => {
+            const req = https.get(url, options);
+
+            req.on('response', async res => {
+                result.headers = res.headers;
+                result.statusCode = <number>res.statusCode;
+                let body = '';
+                res.setEncoding('utf-8');
+
+                for await (const chunk of res) {
+                    body += chunk;
+                }
+
+                resolve(body);
+            });
+
+            req.on('error', (err: any) => {
+                throw new err();
+            });
+        });
+    };
+
+    try {
+        result.data = await requestContent(url);
+        result.success = true;
+        result.failed = false;
+        result.error = '';
+        result.hasError = false;
+    } catch (e: any) {
+        result.data = null;
+        result.success = false;
+        result.failed = true;
+        result.error = e.message;
+        result.hasError = true;
+    }
+
+    return result;
+}
